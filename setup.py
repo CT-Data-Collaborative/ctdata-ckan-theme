8d57d4f1eee051e2ae7a9f3872a6103a8d8700c5
Yauheni Kartavykh <kartavykh.yauheni@qweeco.com>
2014-09-10 14:51:15 +0300

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(
+    name='ckanext-ctdata_theme',
+    version=version,
+    description="CTData ckan theme",
+    long_description='''
+    ''',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Yauheni Kartavykh',
+    author_email='yauheni.kartavykh@qweeco.com',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.ctdata_theme'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points='''
+        [ckan.plugins]
+        ctdata_theme=ckanext.ctdata_theme.plugin:CTDataThemePlugin
+        # Add plugins here, e.g.
+        # myplugin=ckanext.ctdata_theme.plugin:PluginClass
+    ''',
+)
