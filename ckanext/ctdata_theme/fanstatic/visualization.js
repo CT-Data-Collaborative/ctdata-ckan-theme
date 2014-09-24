1c8336a1292bba2dc4588a2bf23949cd05f1fe02
Ben <benwjoyce@gmail.com>
2014-09-24 17:36:41 -0400

    Get filters from checkboxes

--- ckanext/ctdata_theme/fanstatic/visualization.js
+++ ckanext/ctdata_theme/fanstatic/visualization.js
@@ -1,3 +1,11 @@
+var default_filters = ["Ashford", "Ansonia", "New Haven", "2008", "2009", "2010", "2011", "1", "Percent", "Students absent 20 or more days"];
+
+function check_defaults(){
+  $.each(default_filters, function(i){
+    $("li.filter").find("input[value='"+default_filters[i]+"']").prop('checked', true);
+    });
+}
+
 function get_filters(){
   var filters = [];
   dimensions = $("li.filter");
@@ -11,25 +19,21 @@ function get_filters(){
       cur_filter['values'].push(checked[option].value);
     });
     
-    filters.push(cur_filter);
+    if(checked.length != 0)
+      filters.push(cur_filter);
   });
   return filters;
 }
 
 $(function () {
+    check_defaults()
     var dataset_id = $("#dataset_id").val(),
         dataset_title = $("dataset_title").val();
 
     $.ajax({type: "POST",
             url: "/data/" + dataset_id,
             data: JSON.stringify({view: 'chart',
-                                  filters: [{'field': 'Year', 'values': ['2008', '2009', '2010', '2011', '2012', '2013']},
-                                             {'field': 'Town', 'values': ['Andover', 'Ansonia', 'New Haven']},
-                                             {'field': 'Measure Type', 'values': ['Number']},
-                                             {'field': 'Variable', 'values': ['Proficient or Above']},
-                                             {'field': 'Subject', 'values': ['Reading']},
-                                             {'field': 'Grade', 'values': ['Grade 3']},
-                                             {'field': 'Race', 'values': ['all']}]
+                                  filters: get_filters()
                                   }),
             contentType: 'application/json; charset=utf-8'}).done(function(data) {
         var series = data['data'],
