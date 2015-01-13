f6853e1f6bf41e99152137f98a6b71d1a7f0d036
Kate Kasinskaya <kasinskayakate@gmail.com>
2015-01-13 16:18:13 +0300

    add tooltips for datasets on data by topic page

--- ckanext/ctdata_theme/ctdata/topic/services.py
+++ ckanext/ctdata_theme/ctdata/topic/services.py
@@ -3,7 +3,7 @@ import ckan.plugins.toolkit as toolkit
 
 from ..utils import dict_with_key_value
 from ..visualization.services import DatasetService
-
+from IPython import embed
 
 class TopicSerivce(object):
     @staticmethod
@@ -38,7 +38,11 @@ class TopicSerivce(object):
                         subdomain = extra['value']
 
                 if domain and subdomain and action not in hidden_list:
-                    dataset_obj = {'name': dataset['name'], 'title': dataset['title'], 'id': dataset['id']}
+                    help_info = filter(lambda x: x['key'] == 'Help', dataset['extras'])
+                    help_str = help_info[0]['value'] if help_info else ''
+
+                    dataset_obj = {'name': dataset['name'], 'title': dataset['title'],
+                                   'id': dataset['id'], 'help': help_str}
                     dmn = dict_with_key_value('title', domain, domains)
                     if dmn:
                         subdmn = dict_with_key_value('title', subdomain, dmn['subdomains'])
