02cd1351e3c0110b3d281dad27d63264992a3708
kssmilik <kasinskayakate@gmail.com>
2015-04-28 12:20:19 +0300

    try to fix map

--- ckanext/ctdata_theme/fanstatic/leaflet-src.js
+++ ckanext/ctdata_theme/fanstatic/leaflet-src.js
@@ -3593,7 +3593,7 @@ L.Marker = L.Class.extend({
 			if (options.title) {
 				icon.title = options.title;
 			}
-			
+
 			if (options.alt) {
 				icon.alt = options.alt;
 			}
@@ -5083,7 +5083,7 @@ L.Path = (L.Path.SVG && !window.L_PREFER_CANVAS) || !L.Browser.canvas ? L.Path :
 		}
 
 		this._requestUpdate();
-		
+
 		this.fire('remove');
 		this._map = null;
 	},
@@ -8131,7 +8131,7 @@ L.Control.Attribution = L.Control.extend({
 				this.addAttribution(map._layers[i].getAttribution());
 			}
 		}
-		
+
 		map
 		    .on('layeradd', this._onLayerAdd, this)
 		    .on('layerremove', this._onLayerRemove, this);
@@ -9176,5 +9176,45 @@ L.Map.include({
 	}
 });
 
+// print
+
+L.Control.EasyPrint = L.Control.extend({
+    options: {
+        position: 'topleft',
+        title: 'Print map',
+    },
+
+    onAdd: function () {
+        var container = L.DomUtil.create('div', 'leaflet-control-easyPrint leaflet-bar leaflet-control');
+        $('head').append('<style type="text/css" media="print">\
+          html {padding: 0px!important;}\
+          .leaflet-control-easyPrint-button{display: none!important;}\
+          .info h4 {  margin: 0 0 5px; color: #777;}\
+          .legend { line-height: 15px; color: #555;}\
+          .legend i {\
+              width: 15px;\
+              height: 15px;\
+              float: left;\
+              margin-right: 8px;\
+              display: block;\
+              background-size:16px 16px;\
+          }</style>');
+
+        this.link = L.DomUtil.create('a', 'leaflet-control-easyPrint-button leaflet-bar-part', container);
+        this.link.href = 'javascript:void(0)($("#map").print({stylesheet:"/common/css/visualization.css"}))';
+
+        return container;
+    },
+
+
+    _click: function (e) {
+        L.DomEvent.stopPropagation(e);
+        L.DomEvent.preventDefault(e);
+    },
+});
+
+L.easyPrint = function() {
+  return new L.Control.EasyPrint();
+};
 
 }(window, document));
