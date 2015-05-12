68daddcd0e3a2a17146442b6f5860607d96de488
kssmilik <kasinskayakate@gmail.com>
2015-05-12 13:59:43 +0300

    service method creates an array of hashes sorted by the number of filters values matches, and contains additional info about matches

---
+++ ckanext/ctdata_theme/ctdata/compare/services.py
@@ -0,0 +1,53 @@
+import yaml
+import ckan.plugins.toolkit as toolkit
+
+from ..utils import dict_with_key_value
+from ..visualization.services import DatasetService
+from ..community.services import CommunityProfileService
+from IPython import embed
+
+class CompareService(object):
+
+    @staticmethod
+    def get_comparable_datasets(dataset_name):
+      dataset_names = toolkit.get_action('package_list')(data_dict={})
+      main_dataset  = toolkit.get_action('package_show')(data_dict={'id': dataset_name})
+      main_dims     = DatasetService.get_dataset_meta_dimensions(dataset_name)
+      main_geo_type = DatasetService.get_dataset_meta_geo_type(dataset_name)
+      comparable    = []
+
+      for name in dataset_names:
+        dataset  = toolkit.get_action('package_show')(data_dict={'id': name})
+        dims     = DatasetService.get_dataset_meta_dimensions(name)
+        geo_type = DatasetService.get_dataset_meta_geo_type(name)
+
+        if not dataset['private']:
+          dims_matches = filter(lambda dim: dim in main_dims, dims )
+          filters_values_matches = []
+
+          for dim_match in dims_matches:
+            main_f_values_data = DatasetService.get_dataset_meta_field(dataset_name, dim_match, [])
+            main_filter_values = main_f_values_data.split(',') if main_f_values_data != [] else []
+
+            f_values_data = DatasetService.get_dataset_meta_field(name, dim_match, [])
+            filter_values = f_values_data.split(',') if f_values_data != [] else []
+
+            values_matches = filter(lambda f_value: f_value in main_filter_values, filter_values)
+            if values_matches != []:
+              filters_values_matches.append({dim_match: values_matches})
+
+          item = {
+            'dataset_name': name,
+            'geo_type': geo_type,
+            'geo_type_match': main_geo_type == geo_type,
+            'dims_matches_number': len(dims_matches),
+            'dims_matches': dims_matches,
+            'filters_matches_number':len(filters_values_matches),
+            'filters_values_matches': filters_values_matches
+            }
+
+          comparable.append(item)
+
+      comparable = sorted(comparable, key=lambda k: k['filters_matches_number'], reverse=True)
+
+      return comparable
--- ckanext/ctdata_theme/plugin.py
+++ ckanext/ctdata_theme/plugin.py
@@ -59,6 +59,7 @@ class CTDataThemePlugin(plugins.SingletonPlugin):
     def before_map(self, route_map):
         with routes.mapper.SubMapper(route_map, controller='ckanext.ctdata_theme.ctdata.compare.controllers:CompareController') as m:
             m.connect('compare', '/compare', action='compare')
+            m.connect('load_comparable_datasets', '/compare/load_comparable_datasets/{dataset_name}', action='load_comparable_datasets')
 
         with routes.mapper.SubMapper(route_map, controller='ckanext.ctdata_theme.plugin:CTDataController') as m:
             m.connect('news', '/news', action='news')
