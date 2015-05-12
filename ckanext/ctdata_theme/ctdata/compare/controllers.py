902e39765b5e7663e9095c2a3c9eff5a5fba12de
kssmilik <kasinskayakate@gmail.com>
2015-05-12 13:59:43 +0300

    service method creates an array of hashes sorted by the number of filters values matches, and contains additional info about matches

--- ckanext/ctdata_theme/ctdata/compare/controllers.py
+++ ckanext/ctdata_theme/ctdata/compare/controllers.py
@@ -19,28 +19,23 @@ from ..visualization.querybuilders import QueryBuilderFactory
 from ..visualization.views import ViewFactory
 # from services import CommunityProfileService, ProfileAlreadyExists, CantDeletePrivateIndicator
 from ..location.services import LocationService
+from services import CompareService
 
 from IPython import embed
 from termcolor import colored
 
 class CompareController(base.BaseController):
     def __init__(self):
-        self.session = Database().session_factory()
-        self.user_service     = UserService(self.session)
-        self.location_service = LocationService(self.session)
+        self.session         = Database().session_factory()
+        self.user_service    = UserService(self.session)
+        self.compare_service = CompareService
 
     def compare(self):
         dataset_names = toolkit.get_action('package_list')(data_dict={})
         return base.render('compare/compare.html', extra_vars={'dataset_names': dataset_names})
 
-    # def get_topics(self):
-    #     http_response.headers['Content-type'] = 'application/json'
-    #     geography_types = self.location_service.location_geography_types()
-    #     topics          = TopicSerivce.get_topics('community_profile')
-
-
-    #     html  = base.render('communities/snippets/indicator_popup.html', extra_vars={'topics': topics, 'geography_types': geography_types})
-
-    #     return json.dumps({'success': True, 'html': html})
-
-
+    def load_comparable_datasets(self, dataset_name):
+        array = self.compare_service.get_comparable_datasets(dataset_name)
+        embed()
+         # html  = base.render('communities/snippets/indicator_popup.html', extra_vars={'topics': topics, 'geography_types': geography_types})
+        return json.dumps({'success': True, 'html': ''})
