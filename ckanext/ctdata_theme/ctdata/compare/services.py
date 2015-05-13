efbbe0853ea39f673392d484c0f60ea47139f7f9
kssmilik <kasinskayakate@gmail.com>
2015-05-13 13:57:57 +0300

    improve years mathces

--- ckanext/ctdata_theme/ctdata/compare/services.py
+++ ckanext/ctdata_theme/ctdata/compare/services.py
@@ -9,6 +9,26 @@ from IPython import embed
 class CompareService(object):
 
     @staticmethod
+    def compare_years(f_value, main_filter_values):
+      if len(f_value) == 4:
+        return f_value in main_filter_values
+      else:
+        if len(f_value) == 7:
+          splitted  = f_value.split('-')
+          formatted = splitted[0] + '-20' + splitted[1]
+          # print formatted
+          return f_value in main_filter_values or formatted in main_filter_values
+
+        else:
+          if len(f_value) == 9:
+            splitted  = f_value.split('-')
+            formatted = splitted[0] + '-' + splitted[1][-2:]
+            # print formatted
+            return f_value in main_filter_values or formatted in main_filter_values
+
+      return False
+
+    @staticmethod
     def get_comparable_datasets(dataset_name):
       dataset_names = toolkit.get_action('package_list')(data_dict={})
       main_dataset  = toolkit.get_action('package_show')(data_dict={'id': dataset_name})
@@ -26,9 +46,10 @@ class CompareService(object):
           d_years    = DatasetService.get_dataset_meta_field(name, 'Year', [])
           if d_years != []:
             years += ';' + d_years
+
           dims_matches = filter(lambda dim: dim in main_dims, dims )
           filters_values_matches = []
-
+          filters_matches_number = 0
           for dim_match in dims_matches:
             main_f_values_data = DatasetService.get_dataset_meta_field(dataset_name, dim_match, [])
             main_filter_values = main_f_values_data.split(';') if main_f_values_data != [] else []
@@ -36,9 +57,13 @@ class CompareService(object):
             f_values_data = DatasetService.get_dataset_meta_field(name, dim_match, [])
             filter_values = f_values_data.split(';') if f_values_data != [] else []
 
-            values_matches = filter(lambda f_value: f_value in main_filter_values, filter_values)
+            if dim_match == 'Year':
+              values_matches = filter(lambda f_value: CompareService.compare_years(f_value, main_filter_values), filter_values)
+            else:
+              values_matches = filter(lambda f_value: f_value in main_filter_values, filter_values)
             if values_matches != []:
               filters_values_matches.append({dim_match: values_matches})
+              filters_matches_number = filters_matches_number + len(filters_values_matches)
 
           item = {
             'dataset_name': name,
@@ -46,13 +71,13 @@ class CompareService(object):
             'geo_type_match': main_geo_type == geo_type,
             'dims_matches_number': len(dims_matches),
             'dims_matches': dims_matches,
-            'filters_matches_number':len(filters_values_matches),
+            'filters_matches_number': filters_matches_number,
             'filters_values_matches': filters_values_matches
             }
 
           comparable.append(item)
 
       comparable = sorted(comparable, key=lambda k: k['filters_matches_number'], reverse=True)
-      years = list(set(years.split(';')))
+      years = sorted(list(set(years.split(';'))))
 
       return comparable, years
