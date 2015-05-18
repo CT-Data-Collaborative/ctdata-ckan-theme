5b3808d248c6a4a148be465a6d6af13ce6c21935
kssmilik <kasinskayakate@gmail.com>
2015-05-18 15:07:01 +0300

    show dimensions for labels

--- ckanext/ctdata_theme/fanstatic/visualization.js
+++ ckanext/ctdata_theme/fanstatic/visualization.js
@@ -76,7 +76,6 @@ function update_headline_indicators(){
         names_hash[$(this).attr('id')]= $(this).val();
     });
 
-
     $.ajax({type: "POST",
       url: "/dataset/"+dataset_id+"/update_indicators",
       data: JSON.stringify({ names_hash: names_hash,
@@ -745,7 +744,7 @@ function draw_chart(){
           });
 
           name += "</div>";
-          cur_series['name'] = town_name;
+          cur_series['name'] = name;
           cur_series['type'] = display_type;
           cur_legend_series['name'] = town_name;
           series.push(cur_series);
