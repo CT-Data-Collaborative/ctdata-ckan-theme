4428a65702bd7430561aa514c1adad18fa877a93
kssmilik <kasinskayakate@gmail.com>
2015-05-18 12:21:01 +0300

    years matches : creating/editing/removing

--- ckanext/ctdata_theme/fanstatic/compare.js
+++ ckanext/ctdata_theme/fanstatic/compare.js
@@ -1,5 +1,6 @@
 var $main_dataset_select = $("select#dataset_name"),
-    names_hash           = {}
+    names_hash           = {},
+    years_to_remove      = []
 
 
 function load_comparable_datasets(dataset_name){
@@ -22,11 +23,32 @@ $(document).ready(function(){
     $(this).closest('div.modal').modal('hide');
   });
 
+  $('.hide_year').on('click', function(){
+    years_to_remove.push($(this).attr('id'))
+    $(this).closest('tr').hide()
+    $('#update_years').removeClass('disabled').addClass('btn-success');
+  })
+
+  $('.add-year').on('click', function(){
+    $.ajax({type: "POST",
+      url: "/compare/add_year_matches",
+      data: JSON.stringify({ year: $('#new_year').find('input[name=year]').val(),
+        year_matches: $('#new_year').find('input[name=year_matches]').val()}) ,
+      contentType: 'application/json; charset=utf-8',
+      success: function (data) {
+        $('tbody').append(JSON.parse(data).html)
+        $('#message').text('Changes successfully saved.')
+        $('#message_popup').modal('show');
+      }
+    });
+
+  });
+
   $('.edit_matches').click( function(){
     $(this).hide();
     $(this).closest('td').find('span.matches').addClass('hidden')
     $(this).closest('td').find('input.edit_year_matches').removeClass('hidden');
-  })
+  });
 
   $('input.edit_year_matches').on('change', function(){
     names_hash[$(this).attr('id')] = $(this).val();
@@ -37,10 +59,9 @@ $(document).ready(function(){
   });
 
   $('#update_years').on('click', function(){
-    debugger
     $.ajax({type: "POST",
       url: "/compare/update_years_matches",
-      data: JSON.stringify({ names_hash: names_hash}),
+      data: JSON.stringify({ names_hash: names_hash, years_to_remove: years_to_remove}),
       contentType: 'application/json; charset=utf-8',
       success: function (data) {
         $('#update_years').addClass('disabled').removeClass('btn-success');
