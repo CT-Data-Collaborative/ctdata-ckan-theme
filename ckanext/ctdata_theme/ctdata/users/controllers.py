b2919dd912ca9141b43359aa10fcf9211f8ab712
kssmilik <kasinskayakate@gmail.com>
2015-06-12 16:24:56 +0300

    folowee list in user settings

--- ckanext/ctdata_theme/ctdata/users/controllers.py
+++ ckanext/ctdata_theme/ctdata/users/controllers.py
@@ -22,8 +22,12 @@ import ckan.lib.helpers as h
 import ckan.lib.activity_streams as activity_streams
 
 import ckan.logic as logic
+import ckan.new_authz as new_authz
 from pylons import config
-get_action = logic.get_action
+get_action    = logic.get_action
+NotAuthorized = logic.NotAuthorized
+check_access  = logic.check_access
+ValidationError = logic.ValidationError
 
 class UserController(UserController):
     def __init__(self):
@@ -255,3 +259,90 @@ class UserController(UserController):
         http_response.headers['Content-type'] = 'application/json'
         self.session.close()
         return json.dumps({'success': True})
+
+    def read(self, id=None):
+        context = {'model': model, 'session': model.Session,
+                   'user': c.user or c.author, 'auth_user_obj': c.userobj,
+                   'for_view': True}
+        data_dict = {'id': id,
+                     'user_obj': c.userobj,
+                     'include_datasets': True,
+                     'include_num_followers': True}
+
+        context['with_related'] = True
+
+        self._setup_template_variables(context, data_dict)
+
+        # The legacy templates have the user's activity stream on the user
+        # profile page, new templates do not.
+        if h.asbool(config.get('ckan.legacy_templates', False)):
+            c.user_activity_stream = get_action('user_activity_list_html')(
+                context, {'id': c.user_dict['id']})
+        if id == c.userobj.name:
+            return self.dashboard()
+        return base.render('user/read.html')
+
+
+
+    def edit(self, id=None, data=None, errors=None, error_summary=None):
+        context = {'save': 'save' in http_request.params,
+                   'schema': self._edit_form_to_db_schema(),
+                   'model': model, 'session': model.Session,
+                   'user': c.user, 'auth_user_obj': c.userobj
+                   }
+        if id is None:
+            if c.userobj:
+                id = c.userobj.id
+            else:
+                abort(400, _('No user specified'))
+        data_dict = {'id': id}
+
+        try:
+            check_access('user_update', context, data_dict)
+        except NotAuthorized:
+            abort(401, _('Unauthorized to edit a user.'))
+
+        if (context['save']) and not data:
+            return self._save_edit(id, context)
+
+        try:
+            old_data = get_action('user_show')(context, data_dict)
+
+            schema = self._db_to_edit_form_schema()
+            if schema:
+                old_data, errors = validate(old_data, schema)
+
+            c.display_name = old_data.get('display_name')
+            c.user_name = old_data.get('name')
+
+            data = data or old_data
+
+        except NotAuthorized:
+            abort(401, _('Unauthorized to edit user %s') % '')
+        except NotFound:
+            abort(404, _('User not found'))
+
+        user_obj = context.get('user_obj')
+
+        if not (new_authz.is_sysadmin(c.user)
+                or c.user == user_obj.name):
+            abort(401, _('User %s not authorized to edit %s') %
+                  (str(c.user), id))
+
+        errors = errors or {}
+        vars = {'data': data, 'errors': errors, 'error_summary': error_summary}
+
+        self._setup_template_variables({'model': model,
+                                        'session': model.Session,
+                                        'user': c.user or c.author},
+                                       data_dict)
+
+        c.is_myself = True
+        c.show_email_notifications = h.asbool(
+            config.get('ckan.activity_streams_email_notifications'))
+        c.form = base.render(self.edit_user_form, extra_vars=vars)
+        c.followee_list = get_action('followee_list')({'model': model, 'session': model.Session, 'user': c.user or c.author}, {'id': c.userobj.id})
+
+        c.group_followee_list   = filter(lambda i: i['type'] == 'group',  c.followee_list)
+        c.dataset_followee_list = filter(lambda i: i['type'] == 'dataset',  c.followee_list)
+        return base.render('user/edit.html')
