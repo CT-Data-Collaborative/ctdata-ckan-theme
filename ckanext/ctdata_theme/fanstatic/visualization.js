0a568b8e83546327e7b468caeb0c71cd84ca975f
kssmilik <kasinskayakate@gmail.com>
2015-07-06 13:15:54 +0300

    add console.log lines to visualization scrtipt to debug page lock up

--- ckanext/ctdata_theme/fanstatic/visualization.js
+++ ckanext/ctdata_theme/fanstatic/visualization.js
@@ -351,6 +351,7 @@ function display_error(message){
 }
 
 function display_data(){
+    console.log('Start display_data function');
     display_filters();
     display_spinner();
     set_icon(display_type);
@@ -362,15 +363,24 @@ function display_data(){
       new_type = display_type
     }
 
+    console.log('new_type: ' + new_type );
+    console.log('display_type: ' + display_type );
+
     if(disabled.indexOf(new_type) > -1){
+      console.log('throw error: This visualization is disabled for this dataset');
       display_error("This visualization is disabled for this dataset");
       hide_spinner();
       return 0;
     }
 
     towns = $("input[class='" + geography_param.replace(/\s/g, "") + "']:checked");
+    console.log('checked towns:');
+    console.log(towns);
     years = $("input.Year:checked");
     error = ''
+
+    console.log('checked years:');
+    console.log(years);
 
      // collapse_geo = $('#collapse' + geography_param)
     collapse_geo = $("[id='collapse" + geography_param + "']")
@@ -399,6 +409,7 @@ function display_data(){
 
     if(towns.length == 0 && display_type != 'map'){
       if (window.location.href.indexOf(geography_param) > -1) {
+          console.log('update_visualization_link')
           $.ajax({type: "POST",
               url: "/update_visualization_link/"+dataset_id,
               data: JSON.stringify({view: 'table', filters: get_filters()}),
@@ -411,10 +422,12 @@ function display_data(){
 
       hide_spinner();
       error = "Please select a " + geography_param;
+      console.log('Throw error: Please select a' + geography_param)
     }
 
     if (years.length == 0){
       if (window.location.href.indexOf("Year") > -1) {
+        console.log('update_visualization_link')
           $.ajax({type: "POST",
               url: "/update_visualization_link/"+dataset_id,
               data: JSON.stringify({view: 'table', filters: get_filters()}),
@@ -426,11 +439,13 @@ function display_data(){
       }
       hide_spinner();
       error = "Please select a year";
+      console.log('Throw error: Please select a year')
     }
 
     ch_variables = $("input.Variable:checked[value != 'Margins of Error']");
     if(ch_variables.length == 0){
       if (window.location.href.indexOf(geography_param) > -1) {
+          console.log('update_visualization_link')
           $.ajax({type: "POST",
               url: "/update_visualization_link/"+dataset_id,
               data: JSON.stringify({view: 'table', filters: get_filters()}),
@@ -443,8 +458,8 @@ function display_data(){
 
       hide_spinner();
       error = "Please select a Variable" ;
+      console.log('Throw error: Please select a Variable')
     }
-
 
     if (error != ''){
         $("#container_2").html('');
@@ -468,14 +483,19 @@ function display_data(){
       draw_table();
       break;
     default:
+
       //Show the print and save icons
       needed_view_type = 'line_or_chart'
       $(".operations").css("visibility","visible");
       $('.operations').show();
+      console.log(needed_view_type)
+      console.log('Draw table for chart')
       draw_table();
+      console.log('Draw chart')
       draw_chart();
   }
 
+  console.log('Stop display_data function!');
 }
 
 function get_filters(){
@@ -498,7 +518,7 @@ function get_filters(){
 }
 
 function handle_incompatibilities(compatibles){
-
+  console.log(' Start handle incompatibilities: ');
   all_inputs = $("input[type='checkbox'][class != 'indicator_group']");
 
   $.each(all_inputs, function(i){
@@ -515,11 +535,16 @@ function handle_incompatibilities(compatibles){
     }
   });
 
+  console.log('compatibles:');
+  console.log(compatibles);
+  console.log('Finish handle incompatibilities!');
 }
 
 function draw_table(){
+  console.log('Start drawing table')
   var dataset_id = $("#dataset_id").val(),
       dataset_title = $("dataset_title").val();
+  console.log('Start ajax to get vizualization_data')
   $.ajax({type: "POST",
           url: "/vizualization_data/"+dataset_id,
           data: JSON.stringify({view: 'table',
@@ -527,7 +552,7 @@ function draw_table(){
                                omit_single_values: true
                                }),
           contentType: 'application/json; charset=utf-8'}).done(function(data) {
-
+      console.log(data)
       handle_incompatibilities(data['compatibles']);
       if (needed_view_type == 'table')
         change_page_url(data['link']);
@@ -541,6 +566,7 @@ function draw_table(){
       all_dims = data['data'][first_idx]['dims'];
       selected_dims = {};
 
+    console.log('Start process data for table')
      $.each(data['data'], function(d){
         $.each(all_dims, function(dim_name){
           if(data['data'][d]['dims']){
@@ -638,7 +664,9 @@ function draw_table(){
             html += "<td class='col-" + col_num + "'>" + cur_value + "</td>";
           }
         });
-     html = html+"</tbody></table>";
+      console.log('Finish process data for table')
+      console.log('Insert html table')
+      html = html+"</tbody></table>";
       if (needed_view_type == 'line_or_chart'){
         $("#container_2").html(html);
         $("#container_2").removeClass('hidden')
@@ -662,7 +690,7 @@ function draw_table(){
 
 
       }
-  console.log('here')
+  console.log('Finish drawing table!')
   hide_spinner();
 });
 }
@@ -704,11 +732,13 @@ function change_page_url(link){
   window.history.pushState(title, title, link);
 }
 function draw_chart(){
+  console.log('Start draw chart')
   var dataset_id = $("#dataset_id").val(),
       dataset_title = $("#dataset_title").val(),
       description = $("#profile_info").text(),
       source = $('#Source').text();
   set_chart_checkbox();
+  console.log('start ajax call to get vizualization_data')
   $.ajax({type: "POST",
             url: "/vizualization_data/" + dataset_id,
             data: JSON.stringify({view: display_type,
@@ -716,6 +746,8 @@ function draw_chart(){
                                   omit_single_values: true
                                   }),
             contentType: 'application/json; charset=utf-8'}).done(function(data) {
+        console.log('Ajax call done.');
+        console.log(data)
         change_page_url(data['link']);
         var checked_measure = $('input:checked', $('#collapseMeasureType'))[0]
         var type            = checked_measure.value;
@@ -724,6 +756,7 @@ function draw_chart(){
         var years           = data['years'];
         var series_data     = data['data'];
 
+        console.log('start process visualization data for chart')
         $.each(series_data, function(i){
           if (!series_data[i]['dims'])
             return "This series doesn't exist"
@@ -777,6 +810,8 @@ function draw_chart(){
           legend_series.push(cur_legend_series);
         });
 
+        console.log('finish process visualization data for chart!')
+
         if(series.length == 0) {
           hide_spinner();
           return display_error("No results, please select different filters");
@@ -787,6 +822,7 @@ function draw_chart(){
 
         hide_spinner();
 
+        console.log('Draw highchart!');
         $('#container').highcharts({
             chart: { backgroundColor:'transparent' },
             title: {
@@ -866,6 +902,8 @@ function draw_chart(){
             }
         });
     });
+
+  console.log('Finish drawing chart')
 }
 
 function display_spinner(){
@@ -879,7 +917,11 @@ function hide_spinner(){
 }
 
 function display_filters(){
+  console.log('Start display_filters function: ')
   filters = get_filters();
+  console.log('filters: ')
+  console.log(filters)
+
   filter_text = "";
   $.each(filters, function(i){
     if (filters[i]['field'] == geography_param) return "Skip town";
@@ -891,6 +933,7 @@ function display_filters(){
   $li = $('li#Connecticut')
   $li.prependTo($li.closest('ul')[0]);
 
+  console.log('End display_filters function!')
 }
 
 function hide_or_show_clear_link(){
