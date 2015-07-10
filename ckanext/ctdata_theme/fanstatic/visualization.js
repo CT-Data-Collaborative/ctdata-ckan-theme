ef96b64b9fbdca5e62b935043be852ef135d998b
Sasha Cuerda <sasha@mapconomy.com>
2015-07-10 14:37:34 +0200

    tweak to selector

--- ckanext/ctdata_theme/fanstatic/visualization.js
+++ ckanext/ctdata_theme/fanstatic/visualization.js
@@ -237,7 +237,8 @@ function set_filters(display_type){
         console.log($("input[class*='"+column['field']+"']"+"[value='"+column['values'][value]+"']"))
         // $("input[class*='"+column['field']+"']"+"[value='"+column['values'][value]+"']").prop('checked', true);
         // $('input[class*="'+column['field']+ '"]'+'[value="'+column['values'][value]+'"]').prop('checked', true);
-        $('input[class*="'+column['field'].replace("'","")+ '"]'+'[value="'+column['values'][value]+'"]').prop('checked', true);
+        colfield = column['field'].replace("'","");
+        $('input[class*="'+colfield+ '"]'+'[value="'+column['values'][value]+'"]').prop('checked', true);
       });
     });
   }
