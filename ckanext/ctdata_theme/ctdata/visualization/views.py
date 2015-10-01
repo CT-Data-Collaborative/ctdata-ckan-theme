06aa3064a353b88b38f3b4393119a7bde7bfa7cf
Sasha Cuerda <sasha@mapconomy.com>
2015-10-01 17:58:00 +0200

    testing logging

--- ckanext/ctdata_theme/ctdata/visualization/views.py
+++ ckanext/ctdata_theme/ctdata/visualization/views.py
@@ -162,10 +162,10 @@ class TableView(View):
             # Bond rating is breaking. Going to return value
             # irrespective of type and see the mayhem ensue.
             try:
+                print "testing logging"
                 current_mt['data'].append(float(row['Value']))
             except ValueError:
-                current_mt['data'].append(float(1000000000))
-                # current_mt['data'].append(None)
+                current_mt['data'].append(None)
         return result
 
 
