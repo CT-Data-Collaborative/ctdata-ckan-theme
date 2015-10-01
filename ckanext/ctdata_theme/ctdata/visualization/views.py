428a9e3a50ccd84c118312745f7e8bbf14f4b429
Sasha Cuerda <sasha@mapconomy.com>
2015-10-01 18:36:46 +0200

    playing w/ querybuilder

--- ckanext/ctdata_theme/ctdata/visualization/views.py
+++ ckanext/ctdata_theme/ctdata/visualization/views.py
@@ -161,7 +161,7 @@ class TableView(View):
 
             try:
                 # current_mt['data'].append(float(row['Value']))
-                current_mt['data'].append(row['Value'])
+                current_mt['data'].append(float(1)
             except ValueError:
                 current_mt['data'].append(None)
         return result
